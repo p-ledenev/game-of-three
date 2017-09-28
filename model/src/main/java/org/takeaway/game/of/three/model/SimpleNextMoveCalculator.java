4619cc4cf4e7020628888ff3592cc70a0572950d
ledenev pavel <pledenev@gmail.com>
2017-09-28 21:21:05 +0300

    calculate algorithm fixed

--- model/src/main/java/org/takeaway/game/of/three/model/SimpleNextMoveCalculator.java
+++ model/src/main/java/org/takeaway/game/of/three/model/SimpleNextMoveCalculator.java
@@ -7,9 +7,9 @@ public class SimpleNextMoveCalculator implements NextMoveCalculator {
 
     @Override
     public int calculate(int inputValue) {
-        if (inputValue + 1 % 3 == 0)
+        if ((inputValue + 1) % 3 == 0)
             return (inputValue + 1) / 3;
-        if (inputValue - 1 % 3 == 0)
+        if ((inputValue - 1) % 3 == 0)
             return (inputValue - 1) / 3;
         return inputValue / 3;
     }
